36d857a412981418ba75b5ecc6c6d96d2b94955d
roberto <robertocgural@gmail.com>
2023-06-11 22:01:45 -0300

    hasta formularios

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -6,14 +6,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
-class UserController extends AbstractController
-{
+class UserController extends AbstractController{
     
-    public function index(): JsonResponse
-    {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/UserController.php',
-        ]);
-    }
+    // public function index(){
+    //    return $this->render('user/users.html.twig');
+    // }
+
+    
 }
