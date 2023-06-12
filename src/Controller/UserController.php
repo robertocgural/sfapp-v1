ad4d26d2bf9eb7b4fe34486172d33ec0d227d396
Roberto <robertocgural@gmail.com>
2023-06-12 14:46:16 -0300

    form-12-06-23

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,14 +3,25 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
 
 class UserController extends AbstractController{
     
     // public function index(){
     //    return $this->render('user/users.html.twig');
     // }
-
+    
+    public function getUsers(EntityManagerInterface $em): Response
+    {
+        dd($em);
+        //$em = $this->getDoctrine()->getManager();
+        $listUsers = $em->getRepository('App::Users')->findAll([], ['name' => 'ASC']);
+        
+        return $this->render('user/users.html.twig', [
+            'listUsers' => $listUsers
+        ]);
+    }
     
 }
